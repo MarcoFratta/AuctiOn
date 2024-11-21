f26d4dd1c6a1032229dd40c659f63a18727218f4
marco <marco.frattarola@studio.unibo.it>
2024-11-21 15:56:07 +0100

    feat: Add user schema validation middleware

---
+++ user-service/src/routes/UserRoutes.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import { validateRequestBody } from "../middlewares/validation";
+import { UserSchema } from "../schemas/User";
+import {UserController} from "../controllers/UserController";
+
+export const createUserRouter = (
+    controller: UserController
+): express.Router => {
+    const router = express.Router();
+
+    // Routes
+    router.get('/', controller.getUsers);
+    router.get('/:id', controller.getUserById);
+    router.post('/', validateRequestBody(UserSchema), controller.createUser);
+    router.put('/:id', validateRequestBody(UserSchema), controller.updateUser);
+    router.delete('/:id', controller.deleteUser);
+
+    return router;
+};
--- user-service/src/schemas/User.ts
+++ user-service/src/schemas/User.ts
@@ -1,6 +1,7 @@
 import { z } from 'zod';
 
 export const UserSchema = z.object({
+    id: z.string().optional(),
     name: z.string(),
     email: z.string().email(),
 });
