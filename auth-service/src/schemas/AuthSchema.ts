70cbb2b54896ebdb451feed88e5c83ec1029c0f9
marco <marco.frattarola@studio.unibo.it>
2024-11-28 15:01:22 +0100

    feat(auth-service): Add custom schemas

---
+++ auth-service/src/schemas/AuthSchema.ts
@@ -0,0 +1,38 @@
+import {z} from "zod";
+
+const passwordMinLength = 8;
+const passwordRegex = new RegExp('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{' + passwordMinLength + ',}$');
+const passwordError = 'Password must be at least 8 characters and contain an uppercase letter, ' +
+    'lowercase letter, and number';
+
+export const userSchema = z.object({
+    id: z.string().optional(),
+    email: z.string().email(),
+    name: z.string().min(1)
+});
+export const registerSchema = userSchema.extend({
+    password: z
+        .string()
+        .min(8)
+        .regex(passwordRegex, {message: passwordError}),
+}).omit({id: true});
+
+export const loginSchema = z.object({
+    email: userSchema.shape.email,
+    password: z.string().min(8)
+});
+
+export const registeredUser = userSchema.extend({
+    pHash: z.string(),
+    id: z.string()
+});
+export const tokenSchema = z.object({
+    token: z.string()
+});
+
+export type RegisterInputData = z.infer<typeof registerSchema>;
+export type LoginInputData = z.infer<typeof loginSchema>;
+export type User = z.infer<typeof registeredUser>;
+export type Token = z.infer<typeof tokenSchema>;
+export type UserOutput = Omit<User, 'pHash'>;
+
