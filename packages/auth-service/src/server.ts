5aa89595745f4339eeb0f3d5ffd32a3cb4a5f397
marco <marco.frattarola@studio.unibo.it>
2025-01-05 16:32:38 +0100

    fix(auth-service): use logger instead of console log

--- packages/auth-service/src/server.ts
+++ packages/auth-service/src/server.ts
@@ -1,17 +1,18 @@
 import app from './App';
 import { config } from './configs/config';
 import { connectToDatabase } from './utils/MongoDB';
+import logger from './utils/Logger';
 
 const port = config.port;
 
 connectToDatabase()
   .then(() => {
     app.listen(port, () => {
-      console.log('Server is running on port 3000');
+      logger.info('Server is running on port ' + port);
     });
   })
   .catch((error) => {
-    console.error(
+    logger.error(
       'Failed to start server due to database connection error:',
       error,
     );
