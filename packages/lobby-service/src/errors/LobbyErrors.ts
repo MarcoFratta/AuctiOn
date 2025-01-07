0ea8c9d059bc5390d0d77b38abb4cef2a843e9c0
marco <marco.frattarola@studio.unibo.it>
2025-01-07 17:45:20 +0100

    fix(lobby-service): add Unauthenticated and Forbidden errors

--- packages/lobby-service/src/errors/LobbyErrors.ts
+++ packages/lobby-service/src/errors/LobbyErrors.ts
@@ -1,78 +1,72 @@
-export class UserNotFoundError extends Error {
-  constructor(email: string) {
-    super(`User with email ${email} not found`);
-  }
-}
-
 export class LobbyNotFoundError extends Error {
   constructor(id: string) {
-    super(`Lobby with id ${id} not found`);
+    super(`Lobby with id ${id} not found`)
   }
 }
 
 export class LobbyFullError extends Error {
   constructor() {
-    super('Lobby is full');
+    super('Lobby is full')
   }
 }
 
 export class PlayerNotFoundError extends Error {
   constructor() {
-    super('Player not found in lobby');
+    super('Player not found in lobby')
   }
 }
 
 export class PlayersNotReadyError extends Error {
   constructor() {
-    super('All players must be ready to start the game');
+    super('All players must be ready to start the game')
   }
 }
 
 export class NotEnoughPlayersError extends Error {
   constructor() {
-    super('Not enough players to start the game');
+    super('Not enough players to start the game')
   }
 }
 
-export class UnauthorizedError extends Error {
-  constructor() {
-    super('Only the lobby creator can perform this action');
+export class ForbiddenError extends Error {
+  constructor(message: string) {
+    super(message)
   }
 }
 
 export class MatchAlreadyInProgressError extends Error {
-  constructor() {
-    super('Match is already in progress');
+  constructor(message: string) {
+    super(message)
   }
 }
 
 export class UserNotAuthenticatedError extends Error {
   constructor() {
-    super('User is not authenticated');
+    super('Login to perform this action')
   }
 }
 
 export class UserAlreadyJoined extends Error {
   constructor() {
-    super('User already joined the lobby');
+    super('User already joined the lobby')
   }
 }
 
 export class ServiceUnavailableError extends Error {
   constructor() {
-    super('Service not available');
+    super('Service not available')
   }
 }
 
 export class UserAlreadyInLobby extends Error {
   constructor(lobbyId: string) {
-    super(`User is already in lobby ${lobbyId}`);
-    this.name = 'UserAlreadyInLobby';
+    super(`User is already in lobby ${lobbyId}`)
+    this.name = 'UserAlreadyInLobby'
   }
 }
 
 export class UserNotInActiveLobby extends Error {
   constructor() {
-    super('User has not joined any lobby');
+    super('User has not joined any lobby')
   }
 }
--- packages/lobby-service/src/middlewares/ErrorsMiddleware.ts
+++ packages/lobby-service/src/middlewares/ErrorsMiddleware.ts
@@ -1,4 +1,5 @@
 import {
+  ForbiddenError,
   LobbyFullError,
   LobbyNotFoundError,
   MatchAlreadyInProgressError,
@@ -6,90 +7,86 @@ import {
   PlayerNotFoundError,
   PlayersNotReadyError,
   ServiceUnavailableError,
-  UnauthorizedError,
   UserAlreadyJoined,
-} from '../errors/LobbyErrors';
-import { NextFunction, Request, Response } from 'express';
-import logger from '../utils/Logger';
+  UserNotAuthenticatedError,
+  UserNotInActiveLobby,
+} from '../errors/LobbyErrors'
+import { NextFunction, Request, Response } from 'express'
+import logger from '../utils/Logger'
 
 // Error Logger Middleware
-export const ErrorLoggerMiddleware = (
-  err: unknown,
-  _req: Request,
-  _res: Response,
-  next: NextFunction,
-): void => {
-  logger.error(err);
-  next(err);
-};
+export const ErrorLoggerMiddleware = (err: unknown, _req: Request, _res: Response, next: NextFunction): void => {
+  logger.error('[Error logger]' + err)
+  next(err)
+}
 
 // Error Handler Middleware for Lobby Errors
-export const LobbyErrorMiddleware = (
-  err: unknown,
-  _req: Request,
-  res: Response,
-  next: NextFunction,
-): void => {
+export const LobbyErrorMiddleware = (err: unknown, _req: Request, res: Response, next: NextFunction): void => {
   if (err instanceof LobbyNotFoundError) {
     res.status(404).json({
-      error: 'Lobby Not Found',
+      error: 'Not Found',
       message: err.message,
-    });
+    })
   } else if (err instanceof LobbyFullError) {
     res.status(400).json({
-      error: 'Lobby Full',
+      error: 'Bad request',
       message: err.message,
-    });
+    })
   } else if (err instanceof PlayerNotFoundError) {
     res.status(404).json({
-      error: 'Player Not Found',
+      error: 'Not Found',
       message: err.message,
-    });
+    })
   } else if (err instanceof PlayersNotReadyError) {
     res.status(400).json({
-      error: 'Players Not Ready',
+      error: 'Bad request',
       message: err.message,
-    });
+    })
   } else if (err instanceof NotEnoughPlayersError) {
     res.status(400).json({
-      error: 'Not Enough Players',
+      error: 'Bad request',
       message: err.message,
-    });
-  } else if (err instanceof UnauthorizedError) {
+    })
+  } else if (err instanceof UserNotAuthenticatedError) {
+    res.status(401).json({
+      error: 'User not authenticated',
+      message: err.message,
+    })
+  } else if (err instanceof ForbiddenError) {
     res.status(403).json({
-      error: 'Unauthorized',
+      error: 'Forbidden',
       message: err.message,
-    });
+    })
   } else if (err instanceof MatchAlreadyInProgressError) {
     res.status(400).json({
-      error: 'Match Already In Progress',
+      error: 'Bad request',
       message: err.message,
-    });
+    })
   } else if (err instanceof ServiceUnavailableError) {
     res.status(503).json({
       error: 'Service Temporary Unavailable',
       message: 'Service is not responding',
-    });
+    })
   } else if (err instanceof UserAlreadyJoined) {
     res.status(400).json({
-      error: 'User Already Joined',
+      error: 'Bad request',
       message: err.message,
-    });
+    })
+  } else if (err instanceof UserNotInActiveLobby) {
+    res.status(400).json({
+      error: 'Bad request',
+      message: err.message,
+    })
   } else {
-    next(err); // Pass to the generic error handler
+    next(err) // Pass to the generic error handler
   }
-};
+}
 
 // Generic Error Handler Middleware
-export const GenericErrorMiddleware = (
-  err: unknown,
-  _req: Request,
-  res: Response,
-  _next: NextFunction,
-): void => {
-  logger.error('Unhandled error:', err);
+export const GenericErrorMiddleware = (err: unknown, _req: Request, res: Response, _next: NextFunction): void => {
+  logger.error('Unhandled error:', err)
   res.status(500).json({
     error: 'Internal Server Error',
     message: 'An unexpected error occurred.',
-  });
-};
+  })
+}
