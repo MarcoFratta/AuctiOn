7e5c620c4ac59d38b2cdbfae58b8adb6acec21f0
marco <marco.frattarola@studio.unibo.it>
2025-01-09 11:59:08 +0100

    refactor(lobby-service): removed errors middlewares from the routes

--- packages/lobby-service/src/routes/LobbyRoutes.ts
+++ packages/lobby-service/src/routes/LobbyRoutes.ts
@@ -2,9 +2,9 @@ import express from 'express'
 import { validateRequestBody, validateRequestParams } from '../middlewares/ValidationMiddleware'
 import { lobbyConfigSchema, lobbyIdSchema, playerStatusSchema } from '../schemas/Lobby'
 import { LobbyController } from '../controllers/LobbyController'
-import { ErrorLoggerMiddleware, GenericErrorMiddleware, LobbyErrorMiddleware } from '../middlewares/ErrorsMiddleware'
 import { UserLobbyRepo } from '../repositories/UserLobbyRepo'
 import { ActiveLobbyMiddleware } from '../middlewares/ActiveLobbyMiddleware'
+
 const userLobbyRepo = new UserLobbyRepo()
 const activeLobbyMiddleware = new ActiveLobbyMiddleware(userLobbyRepo)
 
