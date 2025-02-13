eb8cf80c83c2b2638bfe02e7038f2be5cccf20c3
marco <marco.frattarola@studio.unibo.it>
2025-02-13 22:04:52 +0100

    feat(api-gateway): better error handling when a service is not responding

--- packages/api-gateway/src/controllers/ProxyController.ts
+++ packages/api-gateway/src/controllers/ProxyController.ts
@@ -3,6 +3,7 @@ import { config } from '../configs/Config'
 import { ServiceNotFoundError } from '../errors/LobbyErrors'
 import { Request, Response } from 'express'
 import logger from '@auction/common/logger'
+import * as net from 'node:net'
 
 export class ProxyController {
   createProxy(serviceName: string, ws = false) {
@@ -21,6 +22,12 @@ export class ProxyController {
         proxyReq: fixRequestBody,
         error: (err, req, res) => {
           logger.error(`Error proxying request: ${err}, request url: ${req.url}`)
+          if (res instanceof net.Socket) {
+            // Handle WebSocket errors
+            res.end() // Close the socket connection
+          } else if (!res.headersSent) {
+            res.status(500).json({ error: `Service is unavailable` })
+          }
         },
       },
     })
