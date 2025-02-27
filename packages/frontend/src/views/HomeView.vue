5cc35d979169ab1a2313c34d557d922fe8df2231
marco <marco.frattarola@studio.unibo.it>
2025-02-27 17:21:46 +0100

    feat(frontend): improved the home page

--- packages/frontend/src/views/HomeView.vue
+++ packages/frontend/src/views/HomeView.vue
@@ -1,12 +1,42 @@
-<script lang="ts" setup></script>
-
 <template>
-  <section class="flex center items-center gap-4 justify-center h-screen">
-    <router-link class="text-green-400 hover:text-green-600" to="/create">
-      create lobby
-    </router-link>
-    <router-link class="text-green-400 hover:text-green-600" to="/join"> join lobby </router-link>
-  </section>
+  <div
+    class="min-h-screen w-full flex flex-col items-center justify-center bg-gray-100 text-gray-900 p-6"
+  >
+    <h1 class="text-4xl font-bold mb-6">AuctiOn</h1>
+
+    <div class="flex space-x-4">
+      <LoadingButton v-if="authenticated" text="Go to Lobby" type="submit" @click="goToLobby" />
+      <LoadingButton v-if="authenticated" text="Join Lobby" @click="joinLobby" />
+      <LoadingButton v-if="authenticated" text="Create Lobby" @click="createLobby" />
+      <LoadingButton v-if="!authenticated" text="Login" @click="router.push('/login')" />
+      <LoadingButton v-if="!authenticated" text="Register" @click="router.push('/register')" />
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<script lang="ts" setup>
+import { computed, onMounted, ref } from 'vue'
+import { useRouter } from 'vue-routr'
+import LoadingButton from '@/components/LoadingButton.vue'
+import { useAuth } from '@/composables/useAuth.js'
+import { useAuthStore } from '@/stores/authStore.js'
+
+const router = useRouter)
+const lobbyId = ref(''
+
+const goToLobby = () => router.push('/lobby')const createLobby = () => router.push('/create')
+onst joinLobby = () => router.push(`/join`)
+const authStore = useAuthStore()
+const authenticated = computed(() => authStore.isAuthenticated)
+
+onMounted(() => {
+  useAuth()
+    .refresh()
+    .then(() => {
+      console.log('Authenticated')
+    })
+    .catch((e) => {
+      console.error(e)
+    })
+})
+</script>
