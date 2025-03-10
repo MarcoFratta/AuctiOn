59c8926aea91446eb4afe416fb82a8bcb0230296
marco <marco.frattarola@studio.unibo.it>
2025-03-10 18:48:17 +0100

    feat(frontend): add lobby msg handler composable

---
+++ packages/frontend/src/composables/useLobbyMsgHandler.ts
@@ -0,0 +1,114 @@
+import { useSocketStore } from '@/stores/socketStore.ts'
+import { useLobbyStore } from '@/stores/lobbyStore.ts'
+import type { AuctionMessage } from '@auction/common'
+import * as validator from '@auction/common/validation'
+import * as messages from '@auction/common/messages'
+import { match } from 'ts-pattern'
+import { useRouter } from 'vue-router'
+import { useAlert } from '@/composables/useAlert.ts'
+
+export function useLobbyMsgHandler() {
+  const socketStore = useSocketStore()
+  const lobbyStore = useLobbyStore()
+  const router = useRouter()
+  const alerts = useAlert()
+
+  function connectAndHandle() {
+    try {
+      socketStore.connect(
+        () => console.log('Connected to lobby'),
+        (event: AuctionMessage) => {
+          console.log(`Received event: ${JSON.stringify(event)}`)
+          const ev = validator.validateSchema(messages.typedMessageSchema.shape.type, event.type)
+          match(ev)
+            .with('auction', () => {
+              const msg = validator.validateSchema(messages.auctionMsgSchema, event)
+              lobbyStore.setLobby(msg.auction)
+              lobbyStore.setPlayerInfo(msg.playerInfo)
+              if (msg.auction.startTimestamp) {
+                router.push('/play')
+              }
+            })
+            .with('player-join', () => {
+              const msg = validator.validateSchema(messages.playerJoinSchema, event)
+              lobbyStore.addUser({
+                id: msg.playerId,
+                username: msg.username,
+                connected: false,
+                status: 'waiting',
+              })
+            })
+            .with('player-connected', () => {
+              const msg = validator.validateSchema(messages.playerConnectedMsgSchema, event)
+              const user = lobbyStore.users.find((user) => user.id === msg.playerId)
+              if (user) user.connected = true
+            })
+            .with('player-disconnected', () => {
+              const msg = validator.validateSchema(messages.playerDisconnectedMsgSchema, event)
+              const user = lobbyStore.users.find((user) => user.id === msg.playerId)
+              if (user) user.connected = false
+            })
+            .with('player-status', () => {
+              const msg = validator.validateSchema(messages.playerStatusSchema, event)
+              const user = lobbyStore.users.find((user) => user.id === msg.playerId)
+              if (user) user.status = msg.status
+            })
+            .with('player-leave', () => {
+              const msg = validator.validateSchema(messages.playerLeaveSchema, event)
+              lobbyStore.removeUser(msg.playerId)
+            })
+            .with('player-info', () => {
+              const msg = validator.validateSchema(messages.playerInfoMsgSchema, event)
+              lobbyStore.updateUser(msg.playerId, msg.playerInfo)
+            })
+            .with('auction-deleted', () => {
+              lobbyStore.clearLobby()
+              router.push('/').then(() => {
+                alerts.error('Lobby deleted', '')
+              })
+            })
+            .with('auction-start', () => {
+              const msg = validator.validateSchema(messages.auctionStartMsgSchema, event)
+              lobbyStore.setLobby(msg.auction)
+              router.push('/play')
+            })
+            .with('timer-start', () => {
+              const msg = validator.validateSchema(messages.timerStartMsgSchema, event)
+              lobbyStore.updateTimer(new Date(msg.time))
+            })
+            .with('new-sale', () => {
+              const msg = validator.validateSchema(messages.saleUpdateMsgSchema, event)
+              lobbyStore.setSale({
+                info: msg.info,
+                sellerId: 'not-implemented',
+                startTimestamp: undefined,
+              })
+            })
+            .with('new-bid', () => {
+              const msg = validator.validateSchema(messages.bidUpdateMsgSchema, event)
+              lobbyStore.setBid(msg.bid)
+            })
+            .with('round-end', () => {
+              const msg = validator.validateSchema(messages.roundEndMsgSchema, event)
+              lobbyStore.setLobby(msg.auction)
+              lobbyStore.setPlayerInfo(msg.playerInfo)
+              lobbyStore.resetTimer()
+            })
+            .otherwise(() => {
+              console.error('Unknown event:', event)
+            })
+        },
+        async () => {
+          await alerts.error('Disconnected from lobby', '')
+          router.push('/').then(() => lobbyStore.clearLobby())
+        },
+        (error) => console.error('Error:', error),
+      )
+    } catch (e) {
+      console.error('Error connecting to lobby:', e)
+      router.push('/join')
+    }
+  }
+
+  return { connectAndHandle }
+}
