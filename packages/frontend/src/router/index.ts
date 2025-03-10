48f5a4719fbcc7e488ea1b49a4ed8703c91b40cf
marco <marco.frattarola@studio.unibo.it>
2025-03-10 19:15:44 +0100

    feat(frontend): add lobby and play routes

--- packages/frontend/src/router/index.ts
+++ packages/frontend/src/router/index.ts
@@ -1,8 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@/views/HomeView.vue'
 import LoginView from '@/views/LoginView.vue'
 import RegisterView from '@/views/RegisterView.vue'
 import CreateLobbyView from '@/views/CreateLobbyView.vue'
+import HomeView from '@/views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -32,7 +32,16 @@ const router = createRouter({
       name: 'join',
       component: () => import('@/views/JoinView.vue'),
     },
-    ,
+    {
+      path: '/lobby',
+      name: 'lobby',
+      component: () => import('@/views/LobbyView.vue'),
+    },
+    {
+      path: '/play',
+      name: 'play',
+      component: () => import('@/views/PlayView.vue'),
+    },
   ],
 })
 
