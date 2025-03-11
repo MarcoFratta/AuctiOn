9658b96afd5df11945586f5b3717e48c82722ef9
marco <marco.frattarola@studio.unibo.it>
2025-03-11 17:20:39 +0100

    chore(frontend): add toastify dependency

--- packages/frontend/src/main.ts
+++ packages/frontend/src/main.ts
@@ -6,6 +6,7 @@ import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 import App from './App.vue'
 import router from './router'
+import Vue3Toastify, { type ToastContainerOptions } from 'vue3-toastify'
 
 const app = createApp(App)
 const pinia = createPinia()
@@ -13,5 +14,9 @@ pinia.use(piniaPluginPersistedstate) // Register the plugin
 app.use(VueSocialSharing.default)
 app.use(pinia)
 app.use(router)
+app.use(Vue3Toastify, {
+  autoClose: 3000,
+  // ...
+} as ToastContainerOptions)
 
 app.mount('#app')
