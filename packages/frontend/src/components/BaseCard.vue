a09d6c12bb5c51fe9b8f536ce366ef62a2ba8a48
marco <marco.frattarola@studio.unibo.it>
2025-03-30 16:39:29 +0200

    feat(frontend): add cards components

---
+++ packages/frontend/src/components/BaseCard.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div
+    class="flex flex-col gap-2 lg:gap-3 justify-center items-left bg-app-white dark:bg-black sm:dark:bg-neutral-950 backdrop-blur-md w-full h-fit flex-grow sm:border border-app-violet-900/30 p-2 lg:p-4 xl:p-6 rounded-lg"
+  >
+    <slot />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ packages/frontend/src/components/InnerCard.vue
@@ -0,0 +1,7 @@
+<template>
+  <div
+    class="bg-white dark:bg-neutral-900 border border-gray-200 w-full sm:border border-app-violet-900/30 dark:border-neutral-800 p-2 lg:p-4 rounded-lg"
+  >
+    <slot />
+  </div>
+</template>
