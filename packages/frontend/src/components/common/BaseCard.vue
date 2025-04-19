00bb51f98410d4c6e9589e749f505a8d13497ea0
marco <marco.frattarola@studio.unibo.it>
2025-04-19 18:05:44 +0200

    refactor(frontend): adjust gap spacing in BaseCard component

--- packages/frontend/src/components/common/BaseCard.vue
+++ packages/frontend/src/components/common/BaseCard.vue
@@ -2,7 +2,7 @@
 
 <template>
   <div
-    class="flex flex-col gap-2 lg:gap-3 justify-start items-left bg-app-white dark:bg-black lg:dark:bg-neutral-950 backdrop-blur-md w-full h-fit flex-grow lg:border border-app-violet-900/30 p-2 lg:p-4 xl:p-6 rounded-lg"
+    class="flex flex-col gap-1 justify-start items-left bg-app-white dark:bg-black lg:dark:bg-neutral-950 backdrop-blur-md w-full h-fit flex-grow lg:border border-app-violet-900/30 p-2 lg:p-4 xl:p-6 rounded-lg"
   >
     <slot />
   </div>
