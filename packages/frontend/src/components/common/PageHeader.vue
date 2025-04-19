7dc2753c97b80b6bb0b62a824081f80579dfd8e4
marco <marco.frattarola@studio.unibo.it>
2025-04-19 15:42:23 +0200

    feat(frontend): add SectionHeader component and PageHeader component

---
+++ packages/frontend/src/components/common/PageHeader.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="flex w-full flex-col items-center mb-4 px-2 relative">
+    <!-- Left animated shape -->
+    <div class="absolute top-5 -left-5 opacity-50 hidden md:block">
+      <GameShapes
+        :color="settingsStore.darkMode ? 'violet' : 'default'"
+        animated
+        size="md"
+        type="circle"
+      />
+    </div>
+
+    <!-- Right animated shape -->
+    <div class="absolute top-5 -right-5 opacity-50 hidden md:block">
+      <GameShapes
+        :color="settingsStore.darkMode ? 'fuchsia' : 'default'"
+        animated
+        size="md"
+        type="triangle"
+      />
+    </div>
+
+    <!-- Title and subtitle -->
+    <Title class="text-3xl md:text-4xl mb-2">{{ title }}</Title>
+    <p class="text-gray-600 dark:text-app-violet-200 text-center max-w-md text-sm md:text-base">
+      {{ subtitle }}
+    </p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import Title from '@/components/common/Title.vue'
+import GameShapes from '@/components/icons/GameShapes.vue'
+import { useSettingsStore } from '@/stores/settingsStore.ts'
+
+defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  subtitle: {
+    type: String,
+    required: true,
+  },
+})
+
+const settingsStore = useSettingsStore()
+</script>
---
+++ packages/frontend/src/components/common/SectionHeader.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+import AppIcons from '@/components/icons/AppIcons.vue'
+
+const props = defineProps<{
+  title: string
+  iconName: string
+  iconColor?: string
+}>()
+
+// Default color if not provided
+const color = props.iconColor || 'violet'
+</script>
+
+<template>
+  <div class="flex items-center justify-between">
+    <div class="flex items-center gap-1.5">
+      <AppIcons :color="color" :name="iconName" size="sm" />
+      <h2 class="text-base font-semibold text-zinc-900 dark:text-white">{{ title }}</h2>
+    </div>
+
+    <!-- Slot for right-side content -->
+    <slot></slot>
+  </div>
+</template>
